46ee91440c82eb8a6cf1ec5f59dc51f64794c7d1
Terin Sebastin <terin01@gmail.com>
2022-07-19 03:22:24 +0100

    Submitting forms

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -38,6 +38,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState?.validate();
+    if (!isValid!) {
+      return;
+    }
     _form.currentState?.save();
     debugPrint(_editedProduct.description);
     debugPrint(_editedProduct.title);
@@ -66,6 +70,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   textInputAction: TextInputAction.next,
                   onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please provide a value';
+                    }
+                    return null;
                   },
                   onSaved: (value) {
                     _editedProduct = Product(
