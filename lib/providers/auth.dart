d15d4c068065734b4795975192b93f77f404d5db
Terin Sebastin <terin01@gmail.com>
2022-08-25 01:11:51 +0100

    Added Login/Signup logic

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -5,15 +5,16 @@ import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
   // ignore: unused_field
-  late String? _token;
+  late String _token;
   // ignore: unused_field
-  late DateTime? _expiryDate;
+  late DateTime _expiryDate;
   // ignore: unused_field
-  late String? _userId;
+  late String _userId;
 
-  Future<void> signup(String email, String password, String? authData) async {
+  Future<void> _authenticate(
+      String? email, String? password, String? urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB01w99Ydp0tV2qLXFItFKVyR3W8AAhxgU');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB01w99Ydp0tV2qLXFItFKVyR3W8AAhxgU');
     final response = await http.post(
       url,
       body: json.encode(
@@ -26,5 +27,13 @@ class Auth with ChangeNotifier {
     );
     // ignore: avoid_print
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String? email, String? password, String? authData) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String? email, String? password, String? authData) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
