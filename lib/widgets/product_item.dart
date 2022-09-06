0b4046413c1e68a71f92969b25aeff9f21872f2d
Terin Sebastin <terin01@gmail.com>
2022-09-06 03:24:41 +0100

    Added Hero widget

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -60,13 +60,16 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                   arguments: product.id);
             }),
-            child: FadeInImage(
-              placeholder:
-                  const AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(
-                product.imageUrl,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    const AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(
+                  product.imageUrl,
+                ),
+                fit: BoxFit.cover,
               ),
-              fit: BoxFit.cover,
             )),
       ),
     );
